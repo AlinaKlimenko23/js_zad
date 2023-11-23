37f77be01b99cf9f4cc29e71ce26560bf52c23ba
AlinaKlimenko23 <151527055+AlinaKlimenko23@users.noreply.github.com>
2023-11-23 15:44:02 +0900

    Add files via upload

---
+++ aaaaaaaaaaa/1.3.js
@@ -0,0 +1,22 @@
+const carName="Ака"
+let maxCarSpeed="9000км.ч"
+let carOwner="Алинча"
+
+alert("Название машины: " + carName);
+alert("Максимальная скорость: "+ maxCarSpeed);
+alert("Владелец машины: "+carOwner)
+
+maxCarSpeed="10км.ч"
+console.log('Умашины изменилась скорость', maxCarSpeed);
+carOwner="Абоба"
+console.log('Умашины изменился владелец', carOwner);
+
+maxCarSpeed="100000000км.ч"
+console.log('Умашины изменилась скорость', maxCarSpeed);
+carOwner="Абебва"
+console.log('Умашины изменился владелец', carOwner);
+
+maxCarSpeed="0,00000001км.ч"
+console.log('Умашины изменилась скорость', maxCarSpeed);
+carOwner="Пупа"
+console.log('Умашины изменился владелец', carOwner)
---
+++ aaaaaaaaaaa/Задание 2/1.2.js
@@ -0,0 +1,7 @@
+const carName="Ака"
+let maxCarSpeed="9000км.ч"
+let carOwner="Алинча"
+
+alert("Название машины: " + carName);
+alert("Максимальная скорость: "+ maxCarSpeed);
+alert("Владелец машины: "+carOwner)
